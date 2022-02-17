2be6e11331801064ef5fd971bb51408b091605f1
Baek-JiYun <89393491+Baek-JiYun@users.noreply.github.com>
2022-02-17 10:06:50 +0900

    게시판기능까지

---
+++ Spring_Board_PJ/src/main/java/kr/co/vo/Criteria.java
@@ -0,0 +1,14 @@
+package kr.co.vo;
+
+public class Criteria {
+
+	private int page;
+	private int perPageNum;
+	private int rowStart;
+	private int rowEnd;
+
+	public Criteria() {
+		this.page = 1;
+		this.perPageNum = 10;
+	}
+}
